d81e9358cec09b312bb020f52f4faaa9ced99615
Nahuel Gonzalez <nahugonza198@gmail.com>
2022-03-15 22:09:25 -0300

    Desafio N2 Navbar

---
+++ OneDrive/Escritorio/Coder/ReactJS/ecommers/src/App.js
@@ -0,0 +1,15 @@
+import NavBar from './components/Navbar';
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">        
+          <NavBar></NavBar>
+    </div>
+  );
+}
+
+export default App;
